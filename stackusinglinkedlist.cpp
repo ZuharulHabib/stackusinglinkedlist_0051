b31efc9d95baedd16f919c427016fbfaa75d8dab
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-05-10 14:52:31 +0700

    menbuat program utama

---
+++ stackusinglinkedlist.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Node {
+public:
+	int data;
+	Node* next;
+
+	Node() {
+		next = NULL;
+	}
+};
+
+
+class Stack {
+private:
+	Node* top;
+
+public:
+	Stack() {
+		top = NULL;
+	}
+
+
+	int push(int value) {
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push value: " << value << endl;
+		return value;
+	}
+
+
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+
+	}
+
+
+	void peek() {
+		if (top == NULL) {
+			cout << "List is Empty." << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		} // Return the value of the top mode
+	}
+
+
+	bool isEmpty() {
+		return top == NULL;
+	}
+};
